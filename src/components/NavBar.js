fe9202da01ca76125571dda1a8dc2f945ca8606a
mesh <meshack.korir@student.moringaschool.com>
2023-04-18 16:03:49 +0300

    initialized the app

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import "../css/css/NavBar.css"
+function NavBar() {
+    return (
+        <>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Blog</a>
+                <a href="#">Comment</a>
+            </nav>    
+        </>
+    )
+}
+export default NavBar;
